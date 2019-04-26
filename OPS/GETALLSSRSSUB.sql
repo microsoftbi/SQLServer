ac9c143e2886e92c67200df01fa8ab80e7257592
iHuskies <microsoftbi@163.com>
2019-04-26 17:25:08 +0800

    First version

---
+++ OPS/GETALLSSRSSUB.sql
@@ -0,0 +1,23 @@
+SELECT USR.UserName AS SubscriptionOwner 
+      ,SUB.ModifiedDate 
+      ,SUB.[Description] 
+      ,SUB.EventType 
+      ,SUB.DeliveryExtension 
+      ,SUB.LastStatus 
+      ,SUB.LastRunTime 
+      ,SCH.NextRunTime 
+      --,SCH.Name AS ScheduleName       
+      ,CAT.[Path] AS ReportPath 
+      --,CAT.[Description] AS ReportDescription 
+FROM dbo.Subscriptions AS SUB 
+     INNER JOIN dbo.Users AS USR 
+         ON SUB.OwnerID = USR.UserID 
+     INNER JOIN dbo.[Catalog] AS CAT 
+         ON SUB.Report_OID = CAT.ItemID 
+     INNER JOIN dbo.ReportSchedule AS RS 
+         ON SUB.Report_OID = RS.ReportID 
+            AND SUB.SubscriptionID = RS.SubscriptionID 
+     INNER JOIN dbo.Schedule AS SCH 
+         ON RS.ScheduleID = SCH.ScheduleID 
+ORDER BY USR.UserName 
+        ,CAT.[Path];
