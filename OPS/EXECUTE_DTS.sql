4bacf20cccee85711574975adc5cf2379b186773
Wade <microsoftbi@163.com>
2020-01-09 15:10:19 +0800

    Update EXECUTE_DTS.sql

--- OPS/EXECUTE_DTS.sql
+++ OPS/EXECUTE_DTS.sql
@@ -1,10 +1,21 @@
 Declare @execution_id bigint
-EXEC [SSISDB].[catalog].[create_execution] @package_name=N'<packge name>',
-       @execution_id=@execution_id OUTPUT,
-       @folder_name=N'<folder name>',
-       @project_name=N'<project name>',
-     @use32bitruntime=False,
-       @reference_id=Null
-Declare @retry_count int = 5
-EXEC [SSISDB].[catalog].[start_execution] @execution_id ,
-       @retry_count=@retry_count
+
+EXEC [SSISDB].[catalog].[create_execution] @package_name=N'Package.dtsx',
+	@execution_id=@execution_id OUTPUT,
+	@folder_name=N'XXXXDWH',
+	@project_name=N'Load2XXXX',
+	@use32bitruntime=False,
+	@reference_id=Null
+
+exec [SSISDB].catalog.set_execution_parameter_value 
+	@execution_id=@execution_id, 
+	@object_type= 50, 
+	@parameter_name = N'SYNCHRONIZED', 
+	@parameter_value = 1;
+
+Declare @retry_count int = 0
+
+EXEC [SSISDB].[catalog].[start_execution] @execution_id , @retry_count=@retry_count
+
+
+SELECT [STATUS] FROM [SSISDB].[catalog].[executions] WHERE execution_id= @execution_id
